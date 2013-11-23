407da1d1c01ece8b6885a15dcee47d6971555a04
Yuan Li <yli@pacificbiosciences.com>
2013-11-22 17:13:09 -0800

    Add support for both hdf 1.8.4 and hdf 1.8.12   bug 18909
    [git-p4: depot-paths = "//depot/software/assembly/cpp/": change = 129385]

--- common/data/hdf/HDFAttributable.h
+++ common/data/hdf/HDFAttributable.h
@@ -9,13 +9,23 @@
 using namespace std;
 using namespace H5;
 
+#ifdef __H5Location_H
+void CallStoreAttributeName(H5Location &obj, string attrName, void *attrListPtr);
+#else
 void CallStoreAttributeName(H5Object &obj, string attrName, void *attrListPtr);
+#endif
 
 class HDFAttributable {
  public:
 	vector<string> attributeNameList;
 
+ private:
+
+#ifdef __H5Location_H
+	void StoreAttributeNames(H5Location &thisobject, vector<string> &attributeNames) {
+#else
 	void StoreAttributeNames(H5Object &thisobject, vector<string> &attributeNames) {
+#endif
 		void *destAndData[2];
 		int nAttr = thisobject.getNumAttrs();
 		unsigned int bounds[2];
@@ -23,9 +33,10 @@ class HDFAttributable {
 		bounds[1] = nAttr;
 		attributeNameList.clear();
 		thisobject.iterateAttrs(&CallStoreAttributeName, 
-														bounds, (void*) &attributeNames);
+								bounds, (void*) &attributeNames);
 	}
 
+ public:
   virtual H5Object* GetObject() {
     return NULL;
   }
@@ -33,7 +44,12 @@ class HDFAttributable {
 	int ContainsAttribute(string attributeName) {
 		int i;
     vector<string> tmpAttributeNames;
+#ifdef __H5Location_H
+    H5Location *obj = GetObject();
+#else
     H5Object *obj = GetObject();
+#endif
+
     assert(obj != NULL);
     StoreAttributeNames(*obj, tmpAttributeNames);
 		for (i = 0; i < tmpAttributeNames.size(); i++) {
@@ -44,7 +60,11 @@ class HDFAttributable {
 
 };
 
+#ifdef __H5Location_H
+void CallStoreAttributeName(H5Location &obj, string attrName, void *attrList){ 
+#else
 void CallStoreAttributeName(H5Object &obj, string attrName, void *attrList){ 
+#endif
 	((vector<string>*)attrList)->push_back(attrName);
  }
 
