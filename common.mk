23253fc9a88f09d8ccd322a4042a6204d4266866
Herb Maeder <hmaeder@pacificbiosciences.com>
2015-07-14 04:43:12 -0800

    build: ml: bug 26194: mobs support in blasr makefiles
    This adds support for mobs to control the blasr makefile from the
    make command line and adds support for building blasr using shared
    libraries for its dependencies.
    
    This basically just copies local copy of common.mk that the mobs build
    was using, so the mobs part of it will still work the same.
    
    Please review this change before I check it in.  It should not affect
    the current default operation of the makefile, it mostly just exposes
    variables that can be controlled from the command line. 
    
    NOTE: I need to make a similar change to pbdagcon.  But since that is
          now a github only project, perhaps we can discuss how to best
          make/review that change.
    
    #review-154315
    @cdunn
    @yli
    @jdrake

--- common.mk
+++ common.mk
@@ -12,27 +12,47 @@ LIBPBIHDF_INCLUDE := $(PBINCROOT)/hdf
 LIBPBDATA_INCLUDE := $(PBINCROOT)/pbdata
 PBBAM_INCLUDE     := $(PBBAM)/include
 HTSLIB_INCLUDE    := $(PBBAM)/../htslib/htslib
+HDF5_INCLUDE      := $(HDF5_INC)
+ZLIB_INCLUDE      := $(ZLIB_ROOT)/include
 
 LIBBLASR_LIB  := $(PBINCROOT)/alignment
 LIBPBIHDF_LIB := $(PBINCROOT)/hdf
 LIBPBDATA_LIB := $(PBINCROOT)/pbdata
 PBBAM_LIB     := $(PBBAM)/lib
 HTSLIB_LIB    := $(PBBAM)/../htslib
+HDF5_LIB      := $(HDF5_LIB)
+ZLIB_LIB      := $(ZLIB_ROOT)/lib
+
+
+LIBPBIHDF_CPP_LIBFLAG := -lhdf5_cpp
+LIBPBIHDF_LIBFLAG     := -lhdf5
+
+LIBBLASR_LIBFLAGS  := -lblasr
+LIBPBIHDF_LIBFLAGS := -lpbihdf
+LIBPBDATA_LIBFLAGS := -lpbdata
+PBBAM_LIBFLAGS     := -lpbbam
+HTSLIB_LIBFLAGS    := -lhts
+HDF5_LIBFLAGS      := $(LIBHDF5_CPP_LIBFLAG) $(LIBHDF5_LIBFLAG)
+ZLIB_LIBFLAGS      := -lz
+
 
 INCDIRS = -I$(LIBBLASR_INCLUDE) \
           -I$(LIBPBIHDF_INCLUDE) \
           -I$(LIBPBDATA_INCLUDE) \
-          -I$(HDF5_INC)
+          -I$(HDF5_INCLUDE)
 
 LIBDIRS = -L$(LIBBLASR_LIB) \
           -L$(LIBPBIHDF_LIB) \
           -L$(LIBPBDATA_LIB) \
           -L$(HDF5_LIB) 
 
-
 ifneq ($(ZLIB_ROOT), notfound)
-	INCDIRS += -I$(ZLIB_ROOT)/include
-	LIBDIRS += -L$(ZLIB_ROOT)/lib
+        # NOTE: The zlib include directory is not needed since we do not
+        #       refer to any zlib include files directly in the blasr source
+        #       code.   But we do need to link to the zlib library (static
+        #       or shared), since htslib depends on it.
+        #          INCDIRS += -I$(ZLIB_INCLUDE)
+	LIBDIRS += -L$(ZLIB_LIB)
 endif
 
 CXXOPTS := -std=c++0x -pedantic \
@@ -44,18 +64,26 @@ DEBUGCXXFLAG := -g -ggdb -fno-inline
 PROFILECXXFLAG := -Os -pg 
 GCXXFLAG := -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -fno-omit-frame-pointer 
 
-LIBS := -lblasr -lpbihdf -lpbdata
-PBBAMLIBS := -lpbbam
-ifneq ($(wildcard "$(HTSLIB_LIB)/libhts.a"),"")
-	PBBAMLIBS += $(HTSLIB_LIB)/libhts.a
+LIBS := $(LIBBLASR_LIBFLAGS) $(LIBPBIHDF_LIBFLAGS) $(LIBPBDATA_LIBFLAGS)
+PBBAMLIBS :=  $(PBBAM_LIBFLAGS)
+ifneq ($(SHARED_LIB),)
+    PBBAMLIBS += $(HTSLIB_LIBFLAGS)
 else
-	PBBAMLIBS += -lhts
+    ifneq ($(wildcard "$(HTSLIB_LIB)/libhts.a"),"")
+        PBBAMLIBS += $(HTSLIB_LIB)/libhts.a
+    else
+         PBBAMLIBS += $(HTSLIB_LIBFLAGS)
+    endif
 endif
 
-ifneq ($(wildcard "$(HDF5_LIB)/libhdf5_cpp.a"),"")
-    LIBS += $(HDF5_LIB)/libhdf5_cpp.a $(HDF5_LIB)/libhdf5.a
+ifneq ($(SHARED_LIB),)
+    LIBS += $(HDF5_LIBFLAGS)
 else
-    LIBS += -lhdf5_cpp -lhdf5 
+    ifneq ($(wildcard "$(HDF5_LIB)/libhdf5_cpp.a"),"")
+        LIBS += $(HDF5_LIB)/libhdf5_cpp.a $(HDF5_LIB)/libhdf5.a
+    else
+        LIBS += $(HDF5_LIBFLAGS)
+    endif
 endif
 
 ifeq ($(origin nopbbam), undefined)
@@ -63,20 +91,36 @@ ifeq ($(origin nopbbam), undefined)
 			   -I$(HTSLIB_INCLUDE) \
 			   -I$(BOOST_INCLUDE)
 
+
 	LIBDIRS += -L$(PBBAM_LIB) \
 			   -L$(HTSLIB_LIB)
 
 	LIBS += $(PBBAMLIBS) 
 endif
 
-LIBS += -lz -lpthread -ldl
+LIBS += $(ZLIB_LIBFLAGS)
+LIBS += -lpthread
+
+# NOTE: libdl is not needed when we are linking with shared libraries, but
+#       is is needed for static linking, since the hdf5 library depends on it
+ifeq ($(SHARED_LIB),)
+    LIBS += -ldl
+endif
+
+ifneq ($(SHARED_LIB),)
+    STATIC :=
+else
+    ifneq ($(OS), Darwin)
+	STATIC := -static
+    else
+	STATIC :=
+    endif
+endif
 
 ifneq ($(OS), Darwin)
 	LIBS += -lrt
-	STATIC := -static
 else
 	LIBS += -lsz
-	STATIC :=
 endif
 
 # -lhdf5, -lhdf5_cpp, -lz required for HDF5
