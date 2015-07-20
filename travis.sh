3599e6e89d9cf5aab01d05f4c06a2efa5c9c0c6c
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-07-20 08:07:33 -0700

    example of temporary fix for static build

--- travis.sh
+++ travis.sh
@@ -3,6 +3,7 @@
 # (or least headers?). But it shows the steps.
 set -ex
 
-./configure.py --sub --no-pbbam HDF5_INCLUDE=${HDF5_INCLUDE} HDF5_LIB=${HDF5_LIB}
+# There is a bug without --shared. Working on it. See #77.
+./configure.py --shared --sub --no-pbbam HDF5_INCLUDE=${HDF5_INCLUDE} HDF5_LIB=${HDF5_LIB}
 make -j4 init-submodule
 make --debug=b -j4 all
